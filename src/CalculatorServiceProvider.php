fafee6e738f3a69fe8ca720c5f50e90792b2573c
hien smb <hien.thai@smbplus.vn>
2025-03-17 13:49:17 +0700

    First release

--- src/CalculatorServiceProvider.php
+++ src/CalculatorServiceProvider.php
@@ -4,6 +4,7 @@ namespace MyVendor\Calculator;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Facades\App;
 
 class CalculatorServiceProvider extends ServiceProvider
 {
@@ -16,14 +17,13 @@ class CalculatorServiceProvider extends ServiceProvider
     public function boot()
     {
         // Đăng ký route từ package
-        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
 
         // Tự động publish Controller vào thư mục `app/Http/Controllers`
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/Controllers/MyPackageController.php' => base_path('app/Http/Controllers/MyPackageController.php'),
-            ], 'controllers');
-            
+                __DIR__.'/Controllers/MyPackageController.php' => App::basePath('app/Http/Controllers/MyPackageController.php'),
+            ], 'controllers'); 
         }
     }
      
